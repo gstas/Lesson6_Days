cd67ff9a494e27a4625ecc6ee78c8052af181fe3
gstas <gstas07@gmail.com>
2019-06-14 23:26:59 +0300

    Add project files.

---
+++ DayDictionary.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Lesson6_Days
+{
+    class DayDictionary
+    {
+        public enum DayOfTheWeek
+        {
+            Mon = 1,
+            Tue,
+            Wed,
+            The,
+            Fri,
+            Sut,
+            Sun
+        }
+
+        public enum Month
+        {
+            Jan = 1,
+            Feb,
+            Mar,
+            Apr,
+            May,
+            Jun,
+            Jul,
+            Aug,
+            Sep,
+            Oct,
+            Nov,
+            Dec
+        }
+
+
+    }
+}
---
+++ DayOfYear.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Lesson6_Days
+{
+    class DayOfYear
+    {
+        ushort year;
+        DayDictionary.Month month;
+        sbyte day;
+        DayDictionary.DayOfTheWeek dayOfTheWeek;
+
+        public ushort Year { get => year; set => year = value; }
+        public sbyte Day { get => day; set => day = value; }
+        internal DayDictionary.Month Month {
+            get => month;
+            set => month = value;
+        }
+        internal DayDictionary.DayOfTheWeek DayOfTheWeek { get => dayOfTheWeek; set => dayOfTheWeek = value; }
+
+        public DayOfYear(ushort year, DayDictionary.Month month, sbyte day, DayDictionary.DayOfTheWeek dayOfTheWeek)
+        {
+            Year = year;
+            Day = day;
+            Month = month;
+            DayOfTheWeek = dayOfTheWeek;
+            
+        }
+
+        public override string ToString()
+        {
+            return $"{Day} {Month} {Year}, {DayOfTheWeek}";
+        }
+    }
+}
